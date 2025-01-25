bb23869a8b3763316d9cf0295c6ea6a9297b6f46
asadullah <asadullahjamilchaudhry@gmail.com>
2025-01-25 23:58:42 +0500

    use of ngFor in ng-template

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,14 +2,26 @@ import {Component} from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { CourseComponent } from "./course/course.component";
 import { NgIf } from '@angular/common';
+import { NgFor } from '@angular/common';
+import { NgForOf } from '@angular/common';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, CourseComponent,NgIf],
+  imports: [RouterOutlet, CourseComponent,NgIf,NgFor,NgForOf],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'ng-content';
-  public myVar = true;
+  public myData = [
+    {name:"asad",age:34,city:"sheikhupura"},
+    {name:"hassan",age:73,city:"gujranwala"},
+    {name:"ali",age:44,city:"lahore"}
+  ]
+
+
+  public myVar = false;
+  click(){
+    return this.myVar === false || this.myVar === true;
+  }
 }
