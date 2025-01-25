c2ea0c9caad8c1d3f4e892185eb8b2e1591c5ec5
asadullah <asadullahjamilchaudhry@gmail.com>
2025-01-25 23:39:09 +0500

    ng-template in Angular.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,15 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { CourseComponent } from "./course/course.component";
+import { NgIf } from '@angular/common';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, CourseComponent],
+  imports: [RouterOutlet, CourseComponent,NgIf],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'ng-content';
+  public myVar = true;
 }
